a161e5abaef6fe1a125ed58c0fd002a0d557e475
Justin <garciajustinkyle229@gmail.com>
2024-03-05 16:14:36 +0800

    register

---
+++ register.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+console.error("ERROR HAPPENED!!!");
+console.warn("Warnings");
+
+//Variables
+//String = "Words" , 'wowowo' , '123'
+//number = 123 ,44e , 2398.12
+//bolean = true , false
+//object = ["apple", "banana", "1,2,3,4"] 
+
+
+
+
+//Declaration of Variable
+let name = "Kyle",
+lastname = "Garcia",
+ age = 18,
+ isProf = true,
+ hobbies = ["PLaying Guitar", "Watching Pool"];
+ run = function (){
+    console.log("Im running");
+ }
+console.log(name,age,isProf,hobbies);
+console.table(hobbies);
+console.log("My name is" + name + " " + lastname);
+console.log(`My name is ${name} ${name}`);
